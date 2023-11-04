57793bda0b23fcb4bc082e33ec3c9440f6cb6a69
Benitha1 <141570388+Benitha1@users.noreply.github.com>
2023-11-04 18:41:24 +0200

    whileloop.java

--- whileloop.java
+++ whileloop.java
@@ -1,10 +1,11 @@
-public class whileloop {
-    public static void main(String args[]) {
+public class whileloop { // this class demonstrates the use of while loop in java
+    public static void main(String args[]) { // main method
+       // declare a variable to storethe current loop iteration 
         int count = 0;
-        while (count < 10) {
-            System.out.print("count:" + count);
+        while (count < 10) { // while loop to count from 1 to 10
+            System.out.print("count:" + count); // print out the loop iteration
             count++;
-            System.out.print("\n");
+            System.out.print("\n"); // jump to the next line
         }
 
     }
