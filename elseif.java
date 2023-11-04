41d7bb23a9c2f6d563be1ae35afbdc9f2a91f94d
Benitha1 <141570388+Benitha1@users.noreply.github.com>
2023-11-04 15:45:51 +0200

    elseif.java

--- elseif.java
+++ elseif.java
@@ -1,14 +1,18 @@
+// this class demonstrates the use of if...else statement in java
 public class elseif {
-    public static void main(String args[]) {
-        int x = 30;
-        if (x == 10) {
-            System.out.print("x is equal to 10");
-        } else if (x == 20) {
-            System.out.print("x is equal to 20");
-        } else if (x == 30) {
-            System.out.print("x is equal to 30");
+    public static void main(String args[]) { // the main method
+        int x = 30; // declare variable
+        
+        if (x == 10) { //check if x is equal to 10,20 or 30
+            System.out.print("x is equal to 10"); // print a message if x is equal to 10
+        }
+        else if (x == 20) { // check if x is equal to 20
+            System.out.print("x is equal to 20"); // //print a message if x is equal to 20
+        } 
+        else if (x == 30) { // check if x is equal to 30
+            System.out.print("x is equal to 30"); // print a message if x is equal to 30
         } else {
-            System.out.println("x is not equal to 12,20 and 30");
+            System.out.println("x is not equal to 12,20 and 30"); // print a message if x is not equal to any of above value
         }
     }
 }
