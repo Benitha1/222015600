6d64a8d021a22b6affee583a4ee52bd28f64f8bc
Benitha1 <141570388+Benitha1@users.noreply.github.com>
2023-11-04 15:35:41 +0200

    Update ifstatement.java

--- ifstatement.java
+++ ifstatement.java
@@ -1,11 +1,14 @@
 public class ifstatement {
+    // this class demonstrates how to use the if statement in java
     public static void main(String args[]) {
+        //declare two variables
         int a = 10;
         int b = 15;
-        if (a > b) {
-            System.out.println("a is greater");
+        
+        if (a > b) { // compare two variables
+            System.out.println("a is greater"); // print a message if a is greater then b
         } else {
-            System.out.println(" b is greater");
+            System.out.println(" b is greater"); // print a message if b is greater then b or equal to a
         }
 
     }
