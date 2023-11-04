9a8b8011739c1db09f80e648b4b12e7d8b353cd7
Benitha1 <141570388+Benitha1@users.noreply.github.com>
2023-11-04 18:29:32 +0200

    nestedforloop.java

--- nestedforloop.java
+++ nestedforloop.java
@@ -1,10 +1,11 @@
+// this class defines a nested for loop
 public class nestedforloop {
-    public static void main(String args[]) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print("*");
+    public static void main(String args[]) { // main method
+        for (int i = 0; i < 5; i++) { // declare a variable as integer i and store outer loop iteration from 1 to 5
+            for (int j = 0; j < 5; j++) { // // declare a variable as integer j and store  inner loop iteration from 1 to 5
+                System.out.print("*"); // print the current loop iteraration
             }
-            System.out.println();
+            System.out.println(); // print a new line character
         }
     }
 }
