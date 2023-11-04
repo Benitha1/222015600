dd53a5c842782b9ff042ccb1336e3c93c4ecd3cf
Benitha1 <141570388+Benitha1@users.noreply.github.com>
2023-11-04 18:04:42 +0200

    breakstatement.java

--- breakstatement.java
+++ breakstatement.java
@@ -1,13 +1,14 @@
+// class to define a break statement
 public class breakstatement {
-    public static void main(String args[]) {
-        for (int j = 10; j <= 20; j++) {
+    public static void main(String args[]) { // mandatory main method
+        for (int j = 10; j <= 20; j++) { // declaration of a variable as integer j and iterate from 10 to 20
 
-            if (j == 15) {
-                // System.out.println(j);
+            if (j == 15) { // if j is equal to 15 , break out the loop
+                
                 break;
-                // System.out.println(j);
+                
             }
-            System.out.println(j);
+            System.out.println(j); // print the value of j
 
         }
 
