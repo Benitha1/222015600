09c652393fbc8c237d339834ce2dbe424be3dde0
Benitha1 <141570388+Benitha1@users.noreply.github.com>
2023-11-04 18:36:38 +0200

    switchcase.java

--- switchcase.java
+++ switchcase.java
@@ -1,24 +1,24 @@
-public class switchcase {
-    public static void main(String[] args) {
-        char grade = 'C';
-        switch (grade) {
+public class switchcase { // this class demonstrates the use of switch case statement in java
+    public static void main(String[] args) { // main method
+        char grade = 'C'; // declare a variable
+        switch (grade) { // switch statement to check the value of the grade variable
             case 'A':
-                System.out.println("Excellent!");
+                System.out.println("Excellent!"); // print the message if the grade is A
                 break;
             case 'B':
-                System.out.println("Well done");
+                System.out.println("Well done"); // print the message if the grade is B
                 break;
             case 'C':
-                System.out.println("Well done");
+                System.out.println("Well done"); // print the message if the grade is C
                 break;
             case 'D':
-                System.out.println("You passed");
+                System.out.println("You passed"); /// print the message if the grade is D
                 break;
             case 'F':
-                System.out.println("Better try again");
+                System.out.println("Better try again"); // print the message if the grade is F
                 break;
             default:
-                System.out.println("Invalid grade");
+                System.out.println("Invalid grade"); // print the message if the grade is invalid
 
         }
     }
