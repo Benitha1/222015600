638391236f6b59da569714f72485619051959803
Benitha1 <141570388+Benitha1@users.noreply.github.com>
2023-11-04 18:10:57 +0200

    compoundinterest.java

--- compoundinterest.java
+++ compoundinterest.java
@@ -1,20 +1,21 @@
 public class compoundinterest {// class name
     public double monthly(double p, double r, double t, double n) {// class method
 
-        return p * Math.pow(1 + (r / n), (n * t));
+        return p * Math.pow(1 + (r / n), (n * t)); // return value
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]) { // mandatory main method
 
         compoundinterest dCompoundinterest = new compoundinterest(); // object name
 
+        // initializing values
         double principle = 500000;
         double rate = 0.18;
         double time = 3;
         double N = 12;
-        double ci = dCompoundinterest.monthly(principle, rate, time, N);
+        double ci = dCompoundinterest.monthly(principle, rate, time, N); // recalling class method
 
-        System.out.println("the compound interest of Mr Bob is" + " " + ci);
+        System.out.println("the compound interest of Mr Bob is" + " " + ci); // output
     }
 
 }
