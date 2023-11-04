f8d85e645f40c56d2d822937e90bcb6ce5d38492
Benitha1 <141570388+Benitha1@users.noreply.github.com>
2023-11-04 18:42:23 +0200

    dowhileloop.java

--- dowhileloop.java
+++ dowhileloop.java
@@ -1,6 +1,6 @@
 public class dowhileloop { // class to define do-while loop
     public static void main(String args[]) { // mandatory main class
-        int count = 20; // 
+        int count = 20; 
         do {
             System.out.println("count: " + count);
             count++;
