e0fe3d67aac959178e61e1bb32ddf1c91023b2b5
Benitha1 <141570388+Benitha1@users.noreply.github.com>
2023-11-04 18:21:04 +0200

    forloop.java

--- forloop.java
+++ forloop.java
@@ -1,7 +1,8 @@
-public class forloop {
-    public static void main(String args[]) {
-        for (int x = 10; x < 20; x = x + 1) {
-            System.out.println("value of x :" + x);
+// this demonstrates the for loop in java
+public class forloop { 
+    public static void main(String args[]) { // the main method
+        for (int x = 10; x < 20; x = x + 1) { // declaring a variable as integer x to store the current loop iteration
+            System.out.println("value of x :" + x); // print the value of the current loop iteration
 
         }
 
